38ff4ec4688a6a352f507f649564200fec96157e
Daniel Schuech <dschuech@gmail.com>
2016-04-11 10:12:47 +0200

    feat: function binding example

--- ng2/upgrade.component.ts
+++ ng2/upgrade.component.ts
@@ -1,81 +1,46 @@
 import {Component, Directive, ElementRef, Input} from 'angular2/core';
 import {UpgradeAdapter} from 'angular2/upgrade';
-import {EventService} from './event.service';
 
 import {ngAdapter} from 'ngAdapter/build/ngAdapter'; 
 
-class ng1TestService {
-  public arg = 'it worked';
-}
-
-HighlightDirective.$inject = ['EventService', 'ng1TestService'];
-function HighlightDirective(event: EventService, test: ng1TestService) {
+function ng1() {
   return {
     scope: {
-      myVar: '='
+      fn: '&'
     },
-    link: (scope: any, element: any, attrs: any) => {
-      scope.myVar = 'newVar'
-      element[0].style.backgroundColor = 'Yellow';
-      
-      event.event.subscribe((data: string) => {
-      element[0].textContent = data;
-      });
-      
-      element[0].addEventListener('mouseenter', () => {
-        element[0].style.backgroundColor = 'Red';
-      });
-      element[0].addEventListener('mouseleave', () => {
-        element[0].style.backgroundColor = 'Yellow';
-      });
-    }
-  }
-}
-
-function HighlightDirective2() {
-  return {
-    link: (scope: any, element: any, attrs: any) => {
-      element[0].style.backgroundColor = 'Green';
+    link: (scope: any, el: Element[], attrs: any) => {
+      scope.fn();
     }
   }
 }
 
 let module = angular.module('newApp', []);
-
-module.service('ng1TestService', ng1TestService); 
-
 let adapter = new ngAdapter(module);
-adapter.upgradeNg1Provider('ng1TestService');
-adapter.addProvider(EventService);
-
-module.controller('ctrl', ['$scope', (scope: any) => {
-    scope.test = 'hello';
-  }])
-  .factory('EventService', <any>adapter.downgradeNg2Provider(EventService));
-
   
-module.directive('myHighlight', HighlightDirective);
-module.directive('myHighlight2', HighlightDirective2);
+module.directive('ng1', ng1);
 
 @Component({
     selector: 'my-ng2-component',
     template: '<h1>My First Angular 2 App</h1>' +
-      '{{testVar}}' +
-      '<div myHighlight [(myVar)]="testVar">ng2Component</div>' +
-      '<div myHighlight2>2. Directive</div>'
+      //Change here callMe to CallMeFn to bind this to this component
+      '<div ng1 [(fn)]="callMe">ng2Component</div>'
     ,
-    providers: [EventService],
-    directives: [adapter.upgradeNg1Directive('myHighlight'),
-      adapter.upgradeNg1Directive('myHighlight2')]
+    directives: [adapter.upgradeNg1Directive('ng1')]
 })
 export class AppComponent {
-  public testVar: string = "myTest"
+  constructor() {
+    console.log('AppComponent this');
+    console.log(this);
+    console.log('---------------------------------------');
+  }
+  callMe() {
+    console.log('callMe this -> should show same instance like AppComponent but is called from another Component')
+    console.log(this);
+    console.log('---------------------------------------');
+  }
+  public callMeFn = this.callMe.bind(this);
 }
 
 module.directive('myNg2Component', <any>adapter.downgradeNg2Component(AppComponent));
 
 adapter.bootstrap(document.body, ['newApp']);
-
-function ng1TestServiceFN() {
-  this.arg = 'A';
-}
